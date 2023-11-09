a8f7d811a579d667095b5c0c9b2aa3af63e046a0
Unique Dismuke <uniquedismuke@Uniques-MacBook-Air.local>
2023-11-08 20:25:29 -0500

    import header sass file and logo then set up nav structure

---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import "./Header.scss"
+import Logo from "../../styles/assets/Logo/InStock-Logo_1x.png"
+
+const Header = () => {
+    return (
+        <header className="navbar">
+            <nav className="navbar__container">
+                <Link to="/">
+                <div className="navbar__logo"> src={Logo} </div>
+                </Link>
+                <div className="navbar__pages">
+                    <Link>
+                    <div className="navbar__page-1"><p>Warehouses</p></div>
+                    </Link>
+                    <Link>
+                    <div className="navbar__page-2"><p>Inventory</p></div>
+                    </Link>
+                </div>
+            </nav>
+        </header>
+    );
+};
+export default Header;
