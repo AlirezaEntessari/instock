ff6b17ccf65bc31c639cfaf7c5bf69ca0bd0c442
Unique Dismuke <uniquedismuke@Uniques-MacBook-Air.local>
2023-11-10 12:36:17 -0500

    work on inventory form

--- src/components/InventoryList/InventoryList.js
+++ src/components/InventoryList/InventoryList.js
@@ -2,10 +2,42 @@ import "./InventoryList.scss"
 import arrow from './../../styles/assets/Icons/chevron_right-24px.svg';
 import del from './../../styles/assets/Icons/delete_outline-24px.svg';
 import edit from './../../styles/assets/Icons/edit-24px.svg';
+import sort from './../../styles/assets/Icons/sort-24px.svg';
+import { Link } from 'react-router-dom';
 
 const InventoryList = ( { inventories }) => {
     console.log(inventories)
     return (
+<>
+        <section>
+            <div className="warehousetopcontainer">
+                <h1>Inventory</h1>
+                <input type="text" img className="warehousetopcontainer__bar" placeholder='   Search...'/>
+                <Link to="/warehouses/add"><button className="warehousetopcontainer__button" input="submit">+ Add New Item</button></Link>
+            </div>
+            <div className="warehousetopcontainer__lower">
+                <div className="warehousetopcontainer__lower--align--one">
+                    <p>WAREHOUSE</p>
+                    <img src={sort} alt="sort" className="warehousetopcontainer__lower--img"/>
+                </div>
+                <div className="warehousetopcontainer__lower--align--two">
+                    <p>ADDRESS</p>
+                    <img src={sort} alt="sort" className="warehousetopcontainer__lower--img"/>
+                </div>
+                <div className="warehousetopcontainer__lower--align--three">
+                    <p>CONTACT NAME</p>
+                    <img src={sort} alt="sort" className="warehousetopcontainer__lower--img"/>
+                </div>
+                <div className="warehousetopcontainer__lower--align--four">
+                    <p>CONTACT INFORMATION</p>
+                    <img src={sort} alt="sort" className="warehousetopcontainer__lower--img"/>
+                </div>
+                <div className="warehousetopcontainer__lower--align-five">
+                    <p>ACTIONS</p>
+                </div>
+            </div>
+        </section>
+
         <section>
             <div className="container">
                 {inventories &&
@@ -20,19 +52,23 @@ const InventoryList = ( { inventories }) => {
                             </div>
                         </div>
                         <div className="inventoryContainer__left--middle">
-                            <h4>ADDRESS</h4>
-                            <h3>{inventory.address}</h3>
+                            <h4>CATEGORY</h4>
+                            <h3>{inventory.category}</h3>
                         </div>
                             <img src={del} alt="delete" className='shrink'/>
                     </div>
                     <div className="inventoryContainer__right">
                         <div className="inventoryContainer__right--start">
-                            <h4>CONTACT NAME</h4>
-                            <h3>{inventory.contact_name}</h3>
+                            <h4>STATUS</h4>
+                            <h3>{inventory.status}</h3>
                         </div>
                         <div className="inventoryContainer__right--middle">
-                            <h4>CONTACT INFORMATION</h4>
-                            <h3>{inventory.phone}{inventory.contact_email}</h3>
+                            <h4>QUANTITY</h4>
+                            <h3>{inventory.quantity}</h3>
+                        </div>
+                        <div className="inventoryContainer__right--middle">
+                            <h4>WAREHOUSE</h4>
+                            <h3>{inventory.warehouse_name}</h3>
                         </div>
                             <img src={edit} alt="edit" className='shrink2'/>
                     </div>
@@ -46,6 +82,7 @@ const InventoryList = ( { inventories }) => {
                 ))}
             </div>
         </section>
+        </>
     )
 }
 
