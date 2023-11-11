92e0639546ac98cd406f9aec331a28a44181c555
Unique Dismuke <uniquedismuke@Uniques-MacBook-Air.local>
2023-11-10 21:44:10 -0500

    create page structure

--- src/components/InventoryDetails/InventoryDetails.js
+++ src/components/InventoryDetails/InventoryDetails.js
@@ -16,7 +16,7 @@ function InventoryDetails(  ) {
     useEffect(() => {
       const getInventory = async (inventoryId) => {
           try {
-              const response = await axios.get(`http://localhost:8080/api/inventories/${inventoryId || 1}`);
+              const response = await axios.get(`http://localhost:3000/api/inventories/${inventoryId || 1}`);
               const info = response.data[0];
               setInventory(info);
               console.log(info);
@@ -27,49 +27,57 @@ function InventoryDetails(  ) {
       getInventory(inventoryId);
   }, [inventoryId]);
 
-
-
-    return (
-        <section>
-            <div className="container">
-                {inventory && (
-                <div key={inventory.id} className="inventoryContainer">
-                    <div className="inventoryContainer__left">
-                        <div className="inventoryContainer__left--start">
-                            <h4>WAREHOUSE</h4>
-                            <div div className="inventoryContainer__left--start--align">
-                            <h2>{inventory.inventory_name}</h2>
-                            <img src={arrow} alt='arrow' className="inventoryContainer__left--img"/>
-                            </div>
-                        </div>
-                        <div className="inventoryContainer__left--middle">
-                            <h4>ADDRESS</h4>
-                            <h3>{inventory.address}</h3>
-                        </div>
-                            <img src={del} alt="delete" className='shrink'/>
-                    </div>
-                    <div className="inventoryContainer__right">
-                        <div className="inventoryContainer__right--start">
-                            <h4>CONTACT NAME</h4>
-                            <h3>{inventory.contact_name}</h3>
-                        </div>
-                        <div className="inventoryContainer__right--middle">
-                            <h4>CONTACT INFORMATION</h4>
-                            <h3>{inventory.phone}{inventory.contact_email}</h3>
-                        </div>
-                            <img src={edit} alt="edit" className='shrink2'/>
-                    </div>
-                    <div className="inventoryContainer__far">
-                        <div className="inventoryContainer__far--align">
-                            <img src={del} alt="delete" className='shrink__hidden'/>
-                            <img src={edit} alt="edit" className='shrink2__hidden'/>
-                        </div>
-                    </div>
+        return (
+            <section>
+                <div>
+                <img src={arrow} alt='arrow'/>
+                <h2>{inventory.inventory_name}</h2>
+                <img src={edit} alt="edit"/>
                 </div>
-                )}
-            </div>
-        </section>
-    )
+            </section>
+        )
+
+    // return (
+    //     <section>
+    //         <div className="container">
+    //             {inventory && (
+    //             <div key={inventory.id} className="inventoryContainer">
+    //                 <div className="inventoryContainer__left">
+    //                     <div className="inventoryContainer__left--start">
+    //                         <h4>WAREHOUSE</h4>
+    //                         <div div className="inventoryContainer__left--start--align">
+    //                         <h2>{inventory.inventory_name}</h2>
+    //                         <img src={arrow} alt='arrow' className="inventoryContainer__left--img"/>
+    //                         </div>
+    //                     </div>
+    //                     <div className="inventoryContainer__left--middle">
+    //                         <h4>ADDRESS</h4>
+    //                         <h3>{inventory.address}</h3>
+    //                     </div>
+    //                         <img src={del} alt="delete" className='shrink'/>
+    //                 </div>
+    //                 <div className="inventoryContainer__right">
+    //                     <div className="inventoryContainer__right--start">
+    //                         <h4>CONTACT NAME</h4>
+    //                         <h3>{inventory.contact_name}</h3>
+    //                     </div>
+    //                     <div className="inventoryContainer__right--middle">
+    //                         <h4>CONTACT INFORMATION</h4>
+    //                         <h3>{inventory.phone}{inventory.contact_email}</h3>
+    //                     </div>
+    //                         <img src={edit} alt="edit" className='shrink2'/>
+    //                 </div>
+    //                 <div className="inventoryContainer__far">
+    //                     <div className="inventoryContainer__far--align">
+    //                         <img src={del} alt="delete" className='shrink__hidden'/>
+    //                         <img src={edit} alt="edit" className='shrink2__hidden'/>
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //             )}
+    //         </div>
+    //     </section>
+    // )
 }
 
 export default InventoryDetails;
