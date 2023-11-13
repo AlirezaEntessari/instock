d750d94f4bde1b786811a65fc84ce159d19c8c1c
AZubaidi <abdulrahman.zubaidi@gmail.com>
2023-11-13 15:20:52 -0500

    finish 3 views, roughly good

--- src/components/WarehouseInventoryList/WarehouseInventoryList.js
+++ src/components/WarehouseInventoryList/WarehouseInventoryList.js
@@ -17,7 +17,6 @@ export default function WarehouseInventoryList () {
               const response = await axios.get(`http://localhost:8080/api/warehouses/${warehouseId}/inventories`);
               const data = response.data;
               setWhInventory(data);
-              console.log(data);
           } catch (error) {
           console.log(error);
           }
@@ -27,29 +26,48 @@ export default function WarehouseInventoryList () {
 
 	return (
 		<div className='wh-invlist'>
+			<div className='wh-invlist__tablet-labels'>
+				<div className='wh-invlist__tablet-labels__div'>
+					<h3>INVENTORY ITEM</h3>
+					<img src="/static/media/sort-24px.68345a3668c9d04480bf63bf24daf75d.svg" alt="sort" className="wh-invlist__tablet-labels__sort"></img>
+				</div>
+				<div className='wh-invlist__tablet-labels__div'>
+					<h3>CATEGORY</h3>
+					<img src="/static/media/sort-24px.68345a3668c9d04480bf63bf24daf75d.svg" alt="sort" className="wh-invlist__tablet-labels__sort"></img>
+				</div>
+				<div className='wh-invlist__tablet-labels__div'>
+					<h3>STATUS</h3>
+					<img src="/static/media/sort-24px.68345a3668c9d04480bf63bf24daf75d.svg" alt="sort" className="wh-invlist__tablet-labels__sort"></img>
+				</div>
+				<div className='wh-invlist__tablet-labels__div'>
+					<h3>QUANTITY</h3>
+					<img src="/static/media/sort-24px.68345a3668c9d04480bf63bf24daf75d.svg" alt="sort" className="wh-invlist__tablet-labels__sort"></img>
+				</div>
+				<h3>ACTIONS</h3>
+			</div>
 			{whInventory &&
 				whInventory.map(invItem => (
 			<div key={invItem.id} className='wh-invlist__item'>
 				<div className='wh-invlist__item__info-div'>
 					<div className='wh-invlist__item__info-div__desc'>
-						<h4>INVENTORY ITEM</h4>
+						<h4 className='wh-invlist__item__label'>INVENTORY ITEM</h4>
 						<div className='wh-invlist__item__info-div__desc__name'>
-							<Link to={`/inventories/${invItem.id}`}><h2>{invItem.item_name}</h2></Link>
+							<Link to={`/inventory/${invItem.id}`}><h2>{invItem.item_name}</h2></Link>
 							<img src={arrow} alt='arrow' className="wh-invlist__item__info-div__desc__arrow"/>
 						</div>
-						<h4 className='wh-invlist__item__info-div__desc__category'>CATEGORY</h4>
+						<h4 className='wh-invlist__item__info-div__desc__category wh-invlist__item__label'>CATEGORY</h4>
 						<h3>{invItem.category}</h3>
 					</div>
 					<div className='wh-invlist__item__info-div__status'>
-						<h4 className='wh-invlist__item__info-div__status__stock'>STATUS</h4>
+						<h4 className='wh-invlist__item__info-div__status__stock wh-invlist__item__label'>STATUS</h4>
 						<h2 className={'wh-invlist__item__info-div__status__stock' + (invItem.quantity > 0 ? '--green' : '--red')}>{invItem.status}</h2>
-						<h4 className='wh-invlist__item__info-div__status__qty'>QTY</h4>
+						<h4 className='wh-invlist__item__info-div__status__qty wh-invlist__item__label'>QTY</h4>
 						<h3>{invItem.quantity}</h3>
 					</div>
 				</div>
 				<div className='wh-invlist__item__actions-div'>
-						<img src={del} alt="delete" className='shrink__hidden'/>
-						<img src={edit} alt="edit" className='shrink2__hidden'/>
+						<img className='wh-invlist__item__actions-div__action' src={del} alt="delete"/>
+						<img src={edit} alt="edit" />
 				</div>
 			</div>
 				))}
