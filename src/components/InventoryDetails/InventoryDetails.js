0fad0d50daab5d7ccaf3e35cbf8365a6161acb16
Unique Dismuke <uniquedismuke@Uniques-MacBook-Air.local>
2023-11-13 21:26:55 -0500

    complete tablet view

--- src/components/InventoryDetails/InventoryDetails.js
+++ src/components/InventoryDetails/InventoryDetails.js
@@ -29,35 +29,45 @@ function InventoryDetails() {
     return (
         <section>
             <div className="inventory-container__top">
-                <img src={arrow} alt='arrow' />
-                <h2>{inventory.item_name}</h2>
-            </div>
-            <div className="inventory-container__button">
-                <button src={edit}></button>
+                <div className="inventory-container__top--one">
+                    <img src={arrow} alt='arrow' />
+                    <h4 className="top__text">Placeholder</h4>
+                    {/* <h2>{inventory.item_name}</h2> */}
+                </div>
+                <div className="inventory-container__top--two"><button src={edit}> Edit </button> </div>
             </div>
             <div className="inventory-container__main">
                 <div className="inventory-container__descript">
                     <h4>ITEM DESCRIPTION:</h4>
-                    <h4>{inventory.description}</h4>
+                    {/* <h4>{inventory.description}</h4> */}
+                    <div className="inventory-container__cat">
+                        <h4>CATEGORY:</h4>
+                        {/* <h4>{inventory.category}</h4> */}
+                    </div>
+                    <div className="inventory-container__hide">
+                            <h4>STATUS:</h4>
+                            {/* <h4>{inventory.status}</h4> */}
+                        </div>
+                        <div className="inventory-container__hide">
+                            <h4>WAREHOUSE:</h4>
+                            {/* <h4>{inventory.warehouse_name}</h4> */}
+                        </div>
                 </div>
-                <div className="inventory-container__cat">
-                    <h4>CATEGORY:</h4>
-                    <h4>{inventory.category}</h4>
-                </div>
+
                 <div className="inventory-container__main--two">
                     <div className="inventory-container__right">
                         <div className="inventory-container__status">
                             <h4>STATUS:</h4>
-                            <h4>{inventory.status}</h4>
+                            {/* <h4>{inventory.status}</h4> */}
                         </div>
                         <div className="inventory-container__ware">
                             <h4>WAREHOUSE:</h4>
-                            <h4>{inventory.warehouse_name}</h4>
+                            {/* <h4>{inventory.warehouse_name}</h4> */}
                         </div>
                     </div>
                     <div className="inventory-container__qty">
                         <h4>QUANTITY:</h4>
-                        <h4>{inventory.quantity}</h4>
+                        {/* <h4>{inventory.quantity}</h4> */}
                     </div>
                 </div>
             </div>
