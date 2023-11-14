69fba6804ec91593d47825fe503466977d0d422c
Unique Dismuke <uniquedismuke@Uniques-MacBook-Air.local>
2023-11-13 23:03:01 -0500

    complese desktop view

--- src/components/InventoryDetails/InventoryDetails.js
+++ src/components/InventoryDetails/InventoryDetails.js
@@ -7,18 +7,18 @@ import axios from 'axios';
 
 function InventoryDetails() {
 
-    const [inventory, setInventory] = useState(null);
+    const [inventory, setInventory] = useState([]);
 
     const { inventoryId } = useParams();
     console.log(inventoryId);
 
     useEffect(() => {
-        const getInventory = async (inventoryId) => {
+        const getInventory = async () => {
             try {
-                const response = await axios.get(`http://localhost:8080/api/inventories/${inventoryId || 1}`);
-                const info = response.data;
-                setInventory(info);
-                console.log(info);
+                const response = await axios.get(`http://localhost:8080/api/inventories/1`);
+                // const info = response.data;
+                setInventory(response);
+                console.log(response);
             } catch (error) {
                 console.log(error);
             }
@@ -32,7 +32,7 @@ function InventoryDetails() {
                 <div className="inventory-container__top--one">
                     <img src={arrow} alt='arrow' />
                     <h4 className="top__text">Placeholder</h4>
-                    {/* <h2>{inventory.item_name}</h2> */}
+                    <h2>{inventory.item_name}</h2>
                 </div>
                 <div className="inventory-container__top--two"><button src={edit}> Edit </button> </div>
             </div>
@@ -44,7 +44,7 @@ function InventoryDetails() {
                         <h4>CATEGORY:</h4>
                         {/* <h4>{inventory.category}</h4> */}
                     </div>
-                    <div className="inventory-container__hide">
+                    <div className="inventory-container__hide--1">
                             <h4>STATUS:</h4>
                             {/* <h4>{inventory.status}</h4> */}
                         </div>
